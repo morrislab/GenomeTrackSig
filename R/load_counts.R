f950babdf05829beefd50ea85d66d1473d5e679b
Cait <cait.harrigan@mail.utoronto.ca>
2019-06-28 10:49:51 -0400

    export loadAndScore

--- R/load_counts.R
+++ R/load_counts.R
@@ -311,3 +311,110 @@ compute_signatures_for_all_examples <- function(countsDir, bootstrapDir, samples
   }
 
 }
+
+#' \code{loadAndScoreIt_pcawg} Take an input vcf file and annotation and generate the counts data.
+#' Create all plotting output that compute_signatures_for_all_examples does.
+#'
+#' @rdname load_counts
+#' @name loadAndScoreIt_pcawg
+#'
+#' @param vcfFile path to variant calling format (vcf) file
+#' @param cnaFile path to copy number abberation (cna) file
+#' @param purityFile path to sample purity file
+#' @param saveIntermediate boolean whether to save intermediate results (mutation types)
+#'
+#' @export
+
+loadAndScoreIt_pcawg <- function(vcfFile, tumortypes, acronym,
+                                 cnaFile = NULL, purityFile = NULL,
+                                 saveIntermediate = F) {
+
+
+  # load_sample() returns (inorder)
+  # list(vcfData, vcf, phis, quadratic_phis, phis_sliding_window, assigns_phylo_nodes,
+  #      assigns_phylo_nodes_sw, acronym, window, tumor_id, phis_for_plot, bootstrap_vcfs, bootstrap_phis))
+
+  tumor_id <- strsplit( unlist(strsplit(vcfFile, "/"))[ length( strsplit(vcfFile, "/")[[1]] ) ] , ".vcf")[[1]]
+
+  if (saveIntermediate){
+    dir.create("intermediateVCAF/", showWarnings = F)
+  }
+
+  list[phis, quadPhis, counts] <- vcfToCounts(vcfFile, cnaFile = cnaFile, purityFile = purityFile,
+                                              refGenome = BSgenome.Hsapiens.UCSC.hg19, saveIntermediate = F,
+                                              intermediateFile = paste0("intermediateVCAF/", tumor_id, "_VCAF.txt"))
+
+  # following checking is all from within compute_signatures_for_all_examples()
+  # will throw a next error if check fails
+
+  if (TrackSig.options()$sig_amount == "onlyKnownSignatures") {
+    # Fit only known signatures
+    list[alex.t, matched_type, acronym] <- TrackSig:::get_signatures_for_current_sample(tumor_id, active_signatures.our_samples, alex, TrackSig.options()$noise_sig)
+  } else {
+    alex.t <- alex
+  }
+
+  if (is.null(acronym) || acronym == "") {
+    print(paste("ERROR: Cancer type not found for ", example))
+    next
+  }
+
+  if (is.null(alex.t))
+  {
+    print(paste0("No active signatures for sample", example, " ...."))
+    next
+  }
+
+  if (is.vector(alex.t)) {
+    next
+  }
+
+  if (sum(counts[apply(alex.t,1,sum) == 0,] != 0) != 0) {
+    print(paste0("Sample ", example, ": some trinucleotides have probability 0 under the model, but their count is non-zero. Sot the count vector is impossible under the model."))
+    next
+  }
+
+  # this is the work-horse part
+
+  dir_name <- paste0(TrackSig.options()$DIR_RESULTS, acronym, "/", tumor_id, "/")
+  suppressWarnings(dir.create(dir_name, recursive = T))
+
+  #method_name <- "iterativeChangePoints"
+
+  if (!file.exists(paste0(dir_name, "mixtures.csv")) || !file.exists(paste0(dir_name, "changepoints.txt")))
+  {
+    if (TrackSig.options()$changepoint_method == "PELT") {
+      list[changepoints, mixtures] <- TrackSig:::find_changepoints_pelt(counts, alex.t, phis, quadPhis)
+    } else {
+      list[bics, optimal, changepoints, mixtures] <- find_changepoints_over_all_signatures_one_by_one(counts, alex.t, n_signatures = ncol(alex.t))
+    }
+
+    write.csv(mixtures, file=paste0(dir_name, "mixtures.csv"))
+
+    n_col <- ifelse(length(changepoints) > 0, length(changepoints), 1)
+    write(changepoints, file=paste0(dir_name, "changepoints.txt"), ncolumns=n_col)
+  } else {
+    mixtures <- TrackSig:::read_mixtures(paste0(dir_name, "mixtures.csv"))
+    cp_file = paste0(dir_name, "changepoints.txt")
+    if (file.info(cp_file)$size == 1) {
+      changepoints <- c()
+    } else {
+      changepoints <- unlist(read.table(cp_file, header=F))
+    }
+  }
+
+
+  plot_name <- paste0(dir_name, "/", acronym, "_", tumor_id, "_", TrackSig.options()$sig_amount, TrackSig.options()$postfix, ".pdf")
+
+  #if (TrackSig.options()$PLOT_FULL_NAME)
+  #{
+  #  plot_name <- paste0(dir_name, "/", acronym, "_", data_method, "_multMix_fittedPerTimeSlice_", sig_amount, "_noPrior_", method_name, postfix, ".pdf")
+  #}
+
+  mark_cp <- !is.null(changepoints)
+  TrackSig:::plot_signatures(mixtures*100, plot_name=plot_name, phis = phis, mark_change_points=mark_cp, change_points=changepoints,
+                             transition_points = NULL,
+                             scale=1.2)
+
+  mixtures.rescaled = NULL
+}
