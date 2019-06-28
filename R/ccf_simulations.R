d3225886cf3a97eeb4aeda3a781c1517cf9fee95
Cait <cait.harrigan@mail.utoronto.ca>
2019-06-28 18:58:37 -0400

    formatting

--- R/ccf_simulations.R
+++ R/ccf_simulations.R
@@ -3,9 +3,9 @@
 #' Functions required for generating simulated data for TrackSig
 #'
 
-#' \code{load_sim_signatures} <man content>
+#' \code{get_alexMerged} <man content>
 #' @rdname ccf_simulations
-load_sim_signatures <- function(signature_file) {
+get_alexMerged <- function(signature_file = TrackSig.options()$signature_file) {
 	sigs_to_merge <- list()
 	sigs_to_merge[["SBS7"]] <- c("SBS7a", "SBS7b", "SBS7c", "SBS7d")
 	sigs_to_merge[["SBS17"]] <- c("SBS17a", "SBS17b")
@@ -57,179 +57,6 @@ save_exposures_per_time_point <- function(data_all_clusters, sig_names, file_pat
 	write.csv(exposures, file = paste0(file_path, "_exposures.txt"), quote=F)
 }
 
-#' Generate simulations for mutations given clusters, their CCFs, CNAs and signatures.
-##' @param n_clusters integer number of clusters of mutations to simulate
-##' @param cluster_ccfs list of cluster mean values
-##' @param n_mut_per_cluster list of number of mutations to generate per cluster
-##' @param sig_activities list of signatures and their activities for each cluster
-##' @param cluster_cna_info list of named list with elements "fractions", \cr
-##' "mut_cn", "total_cn". There should be one inner list per cluster.
-##' @param mean_depth average read depth to simulate> Defaults to 100
-##' @param to_file logical whether to save generated simulations to a file. Defaluts to TRUE
-##' @param simulation_name simulation name to appear in outputted file name. Defaults to "simulation"
-##' @rdname ccf_simulations
-##' @export
-
-# Generate simulations for mutations given clusters, their CCFs, CNAs and signatures.
-generate_ccf_simulation <- function(
-	n_clusters, cluster_ccfs,
-	n_mut_per_cluster, sig_activities,
-	signature_def,
-	cluster_cna_info = list(), mean_depth = 100, to_file = TRUE,
-	simulation_name = "simulation", outdir = ".", bin_size){
-
-	dir.create(outdir, showWarnings = FALSE)
-
-	normal_mut_alleles = 1
-	normal_total_CN = 2
-
-	stopifnot(length(cluster_ccfs) == n_clusters)
-	stopifnot(length(n_mut_per_cluster) == n_clusters)
-	if (length(cluster_cna_info) > 0) {
-		stopifnot(length(cluster_cna_info) == n_clusters)
-	}
-
-	trinucl_names <- rownames(signature_def)
-
-	data_all_clusters <- list()
-	for (cl in 1:n_clusters) {
-		n_mut = n_mut_per_cluster[cl]
-		cluster_ccf = cluster_ccfs[cl]
-
-		cluster_sigs = sig_activities[[cl]]
-		sig_names = names(cluster_sigs)
-		stopifnot(sum(unlist(cluster_sigs)) - 1. < 0.001)
-
-		# Sample depth from Poisson (lambda = mean depth)
-		depths =  rpois(n_mut, lambda = mean_depth) # rep(mean_depth, n_mut)
-		n_alt_alleles <- c()
-		n_ref_alleles <- c()
-
-		if (length(cluster_cna_info) == 0) {
-			# No CNA changes
-			prob = rep(cluster_ccf * (normal_mut_alleles / normal_total_CN), n_mut)
-			mut_alleles = rep(normal_mut_alleles, n_mut)
-			total_CN = rep(normal_total_CN, n_mut)
-		} else {
-			# CNA change
-			current_cluster_cna = cluster_cna_info[[cl]]
-
-			stopifnot(length(current_cluster_cna[["fractions"]]) == length(current_cluster_cna[["mut_cn"]]))
-			stopifnot(length(current_cluster_cna[["fractions"]]) == length(current_cluster_cna[["total_cn"]]))
-			stopifnot(sum(current_cluster_cna[["fractions"]]) == 1.0)
-
-			prob = c()
-			mut_alleles = c()
-			total_CN = c()
-			for (i in 1:length(current_cluster_cna[["fractions"]])) {
-				current_frac = current_cluster_cna[["fractions"]][i]
-				current_mut_cn = current_cluster_cna[["mut_cn"]][i]
-				current_total_cn = current_cluster_cna[["total_cn"]][i]
-
-				n_mut_with_cn_change = as.integer(n_mut * current_frac) + 1
-				prob = c(prob, rep(cluster_ccf * (current_mut_cn / current_total_cn),   n_mut_with_cn_change))
-				mut_alleles = c(mut_alleles, rep(current_mut_cn, n_mut_with_cn_change))
-				total_CN = c(total_CN, rep(current_total_cn, n_mut_with_cn_change))
-			}
-			prob = prob[1:n_mut]
-			mut_alleles = mut_alleles[1:n_mut]
-			total_CN = total_CN[1:n_mut]
-		}
-
-		stopifnot(length(prob) == n_mut)
-
-		# For each mutation in the cluster
-		for (i in 1:n_mut) {
-			# Sample number of variant alleles from a Binomial( d, CCF * (# mutant alleles / avg CN))
-			alt <- rbinom(1, depths[i], prob[i])
-			n_alt_alleles <- c(n_alt_alleles, alt)
-			n_ref_alleles <- c(n_ref_alleles, depths[i] - alt)
-		}
-
-		# Sample mutation types for this cluster
-		if (sum(sig_names %in% colnames(signature_def)) != length(sig_names)) {
-			stop(paste("At least one of the signatures not found: ", toString(sig_names)))
-		}
-
-		mut_types <- c()
-		# Sample mutations from each signature
-		for (i in 1:length(cluster_sigs)) {
-			sig_activity = cluster_sigs[[i]]
-			mut_per_sig = as.integer(n_mut * sig_activity)+1
-
-			sig_def <- signature_def[,names(cluster_sigs)[i]]
-
-			# Get counts over mutation types
-			mut_type_counts <- rmultinom(1, mut_per_sig, unlist(sig_def))
-			rownames(mut_type_counts) <- trinucl_names
-
-			# Create a list of mutation types out of counts
-			mut_types_from_sig <- unlist(sapply(1:length(mut_type_counts), function(ind) {rep(trinucl_names[ind], mut_type_counts[ind]) }))
-			stopifnot(length(mut_types_from_sig) == mut_per_sig)
-
-			mut_types <- c(mut_types, mut_types_from_sig)
-		}
-		mut_types = mut_types[1:n_mut]
-
-		# Shuffle list of mutation types
-		mut_types <- sample(mut_types)
-		stopifnot(length(mut_types) == n_mut)
-
-		#  Report #variant, #reference, and avg CN for each mutation
-		data <- data.frame(n_alt_alleles, n_ref_alleles, total_CN, mut_alleles, mut_types, data.frame(cluster_sigs), stringsAsFactors = F)
-		if ("SBS2.13" %in% colnames(data)) {
-      		colnames(data)[colnames(data) == "SBS2.13"] = "SBS2.13"
-		}
-
-		##################
-		if(TrackSig.options()$simulation_pdf == T){
-
-		  mut_ccfs <- compute_uncorrected_ccf(data$n_alt_alleles, data$n_ref_alleles)
-
-		  pdf(paste0(outdir, "/", simulation_name, "_mut_ccf", cl, ".pdf"), width = 8, height=5)
-		  hist(mut_ccfs, breaks=200)
-		  dev.off()
-		}
-		##################
-
-		data_all_clusters[[cl]] <- data
-	}
-
-	data_all_clusters_list <- data_all_clusters
-	data_all_clusters_table <- do.call(rbind, data_all_clusters)
-	##################
-	if(TrackSig.options()$simulation_pdf == T){
-	  mut_ccfs <- compute_uncorrected_ccf(data_all_clusters_table$n_alt_alleles, data_all_clusters_table$n_ref_alleles)
-
-	  pdf(paste0(outdir, "/", simulation_name, "_mut_ccf.pdf"), width = 8, height=5)
-	  hist(mut_ccfs, breaks=200)
-	  dev.off()
-	}
-	##################
-
-	if (to_file == TRUE){
-		print(paste0("Saving simulation ",simulation_name))
-
-		list[chrom, pos] = generate_chr_pos(n_mut = nrow(data_all_clusters_table))
-		file_path = paste0(outdir, "/", simulation_name)
-		save_as_vcf(data_all_clusters_table,  chrom, pos, file_path)
-
-		sig_data <- data.frame(chrom, pos, data_all_clusters_table[,sig_names])
-		colnames(sig_data) <- c("chromosome", "start", sig_names)
-		write.table(sig_data, file = paste0(file_path, "_sig_exp_per_mut.txt"), sep = "\t", row.names=F, quote=F)
-
-		save_exposures_per_time_point(data_all_clusters_table, sig_names, file_path, bin_size=bin_size)
-
-		write_sim_summary(sim_data_all_clusters = data_all_clusters_list,
-			simulation_name = simulation_name,
-			sig_activities = do.call(rbind,sig_activities),
-			ccfs=cluster_ccfs,
-			mut_per_cluster = n_mut_per_cluster,
-			file_path = file_path)
-	}
-
-	return(data_all_clusters_list)
-}
 
 
 generate_chr_pos <- function(n_mut) {
@@ -356,6 +183,179 @@ write_sim_summary <- function(sim_data_all_clusters, simulation_name, sig_activi
 	write(toString(mut_per_cluster), file_name, append=T)
 }
 
+#' Generate simulations for mutations given clusters, their CCFs, CNAs and signatures.
+##' @param n_clusters integer number of clusters of mutations to simulate
+##' @param cluster_ccfs list of cluster mean values
+##' @param n_mut_per_cluster list of number of mutations to generate per cluster
+##' @param sig_activities list of signatures and their activities for each cluster
+##' @param cluster_cna_info list of named list with elements "fractions", \cr
+##' "mut_cn", "total_cn". There should be one inner list per cluster.
+##' @param mean_depth average read depth to simulate> Defaults to 100
+##' @param to_file logical whether to save generated simulations to a file. Defaluts to TRUE
+##' @param simulation_name simulation name to appear in outputted file name. Defaults to "simulation"
+##' @rdname ccf_simulations
+##' @export
+
+# Generate simulations for mutations given clusters, their CCFs, CNAs and signatures.
+generate_ccf_simulation <- function(
+  n_clusters, cluster_ccfs,
+  n_mut_per_cluster, sig_activities,
+  signature_def,
+  cluster_cna_info = list(), mean_depth = 100, to_file = TRUE,
+  simulation_name = "simulation", outdir = ".", bin_size){
+
+  dir.create(outdir, showWarnings = FALSE)
+
+  normal_mut_alleles = 1
+  normal_total_CN = 2
+
+  stopifnot(length(cluster_ccfs) == n_clusters)
+  stopifnot(length(n_mut_per_cluster) == n_clusters)
+  if (length(cluster_cna_info) > 0) {
+    stopifnot(length(cluster_cna_info) == n_clusters)
+  }
+
+  trinucl_names <- rownames(signature_def)
+
+  data_all_clusters <- list()
+  for (cl in 1:n_clusters) {
+    n_mut = n_mut_per_cluster[cl]
+    cluster_ccf = cluster_ccfs[cl]
+
+    cluster_sigs = sig_activities[[cl]]
+    sig_names = names(cluster_sigs)
+    stopifnot(sum(unlist(cluster_sigs)) - 1. < 0.001)
+
+    # Sample depth from Poisson (lambda = mean depth)
+    depths =  rpois(n_mut, lambda = mean_depth) # rep(mean_depth, n_mut)
+    n_alt_alleles <- c()
+    n_ref_alleles <- c()
+
+    if (length(cluster_cna_info) == 0) {
+      # No CNA changes
+      prob = rep(cluster_ccf * (normal_mut_alleles / normal_total_CN), n_mut)
+      mut_alleles = rep(normal_mut_alleles, n_mut)
+      total_CN = rep(normal_total_CN, n_mut)
+    } else {
+      # CNA change
+      current_cluster_cna = cluster_cna_info[[cl]]
+
+      stopifnot(length(current_cluster_cna[["fractions"]]) == length(current_cluster_cna[["mut_cn"]]))
+      stopifnot(length(current_cluster_cna[["fractions"]]) == length(current_cluster_cna[["total_cn"]]))
+      stopifnot(sum(current_cluster_cna[["fractions"]]) == 1.0)
+
+      prob = c()
+      mut_alleles = c()
+      total_CN = c()
+      for (i in 1:length(current_cluster_cna[["fractions"]])) {
+        current_frac = current_cluster_cna[["fractions"]][i]
+        current_mut_cn = current_cluster_cna[["mut_cn"]][i]
+        current_total_cn = current_cluster_cna[["total_cn"]][i]
+
+        n_mut_with_cn_change = as.integer(n_mut * current_frac) + 1
+        prob = c(prob, rep(cluster_ccf * (current_mut_cn / current_total_cn),   n_mut_with_cn_change))
+        mut_alleles = c(mut_alleles, rep(current_mut_cn, n_mut_with_cn_change))
+        total_CN = c(total_CN, rep(current_total_cn, n_mut_with_cn_change))
+      }
+      prob = prob[1:n_mut]
+      mut_alleles = mut_alleles[1:n_mut]
+      total_CN = total_CN[1:n_mut]
+    }
+
+    stopifnot(length(prob) == n_mut)
+
+    # For each mutation in the cluster
+    for (i in 1:n_mut) {
+      # Sample number of variant alleles from a Binomial( d, CCF * (# mutant alleles / avg CN))
+      alt <- rbinom(1, depths[i], prob[i])
+      n_alt_alleles <- c(n_alt_alleles, alt)
+      n_ref_alleles <- c(n_ref_alleles, depths[i] - alt)
+    }
+
+    # Sample mutation types for this cluster
+    if (sum(sig_names %in% colnames(signature_def)) != length(sig_names)) {
+      stop(paste("At least one of the signatures not found: ", toString(sig_names)))
+    }
+
+    mut_types <- c()
+    # Sample mutations from each signature
+    for (i in 1:length(cluster_sigs)) {
+      sig_activity = cluster_sigs[[i]]
+      mut_per_sig = as.integer(n_mut * sig_activity)+1
+
+      sig_def <- signature_def[,names(cluster_sigs)[i]]
+
+      # Get counts over mutation types
+      mut_type_counts <- rmultinom(1, mut_per_sig, unlist(sig_def))
+      rownames(mut_type_counts) <- trinucl_names
+
+      # Create a list of mutation types out of counts
+      mut_types_from_sig <- unlist(sapply(1:length(mut_type_counts), function(ind) {rep(trinucl_names[ind], mut_type_counts[ind]) }))
+      stopifnot(length(mut_types_from_sig) == mut_per_sig)
+
+      mut_types <- c(mut_types, mut_types_from_sig)
+    }
+    mut_types = mut_types[1:n_mut]
+
+    # Shuffle list of mutation types
+    mut_types <- sample(mut_types)
+    stopifnot(length(mut_types) == n_mut)
+
+    #  Report #variant, #reference, and avg CN for each mutation
+    data <- data.frame(n_alt_alleles, n_ref_alleles, total_CN, mut_alleles, mut_types, data.frame(cluster_sigs), stringsAsFactors = F)
+    if ("SBS2.13" %in% colnames(data)) {
+      colnames(data)[colnames(data) == "SBS2.13"] = "SBS2.13"
+    }
+
+    ##################
+    if(TrackSig.options()$simulation_pdf == T){
+
+      mut_ccfs <- compute_uncorrected_ccf(data$n_alt_alleles, data$n_ref_alleles)
+
+      pdf(paste0(outdir, "/", simulation_name, "_mut_ccf", cl, ".pdf"), width = 8, height=5)
+      hist(mut_ccfs, breaks=200)
+      dev.off()
+    }
+    ##################
+
+    data_all_clusters[[cl]] <- data
+  }
+
+  data_all_clusters_list <- data_all_clusters
+  data_all_clusters_table <- do.call(rbind, data_all_clusters)
+  ##################
+  if(TrackSig.options()$simulation_pdf == T){
+    mut_ccfs <- compute_uncorrected_ccf(data_all_clusters_table$n_alt_alleles, data_all_clusters_table$n_ref_alleles)
+
+    pdf(paste0(outdir, "/", simulation_name, "_mut_ccf.pdf"), width = 8, height=5)
+    hist(mut_ccfs, breaks=200)
+    dev.off()
+  }
+  ##################
+
+  if (to_file == TRUE){
+    print(paste0("Saving simulation ",simulation_name))
+
+    list[chrom, pos] = generate_chr_pos(n_mut = nrow(data_all_clusters_table))
+    file_path = paste0(outdir, "/", simulation_name)
+    save_as_vcf(data_all_clusters_table,  chrom, pos, file_path)
+
+    sig_data <- data.frame(chrom, pos, data_all_clusters_table[,sig_names])
+    colnames(sig_data) <- c("chromosome", "start", sig_names)
+    write.table(sig_data, file = paste0(file_path, "_sig_exp_per_mut.txt"), sep = "\t", row.names=F, quote=F)
+
+    save_exposures_per_time_point(data_all_clusters_table, sig_names, file_path, bin_size=bin_size)
+
+    write_sim_summary(sim_data_all_clusters = data_all_clusters_list,
+                      simulation_name = simulation_name,
+                      sig_activities = do.call(rbind,sig_activities),
+                      ccfs=cluster_ccfs,
+                      mut_per_cluster = n_mut_per_cluster,
+                      file_path = file_path)
+  }
+
+  return(data_all_clusters_list)
+}
 
 ##' \code{create_simulation_set} generates simulated data files
 ##' @rdname create_simulation_set
@@ -371,7 +371,7 @@ create_simulation_set <- function(outdir = "simulations", mut_per_sim = 5000,
 	dir.create(outdir, showWarnings = FALSE)
 	set.seed(2019)
 
-	signature_def = load_sim_signatures(signature_file)
+	signature_def = get_alexMerged(signature_file)
 
 	if (rewrite_annotations) {
 	  # remove simulation annotations (rebuilt upon simulation) and create new ones
