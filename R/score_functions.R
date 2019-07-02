55f9c14bf54b8b1de25daab07320f565c2712e9a
Cait <cait.harrigan@mail.utoronto.ca>
2019-07-01 20:39:09 -0400

    rm bin_size scaling

--- R/score_functions.R
+++ R/score_functions.R
@@ -15,7 +15,7 @@ gaussian_ll <- function(phis, quad_phis, bin_size, ...){
   LL <- sum(log(dnorm(phis, mean = mu, sd = sigma)))
 
   # multiply likelihood by bin_size for scaling
-  return(LL * bin_size)
+  return(LL)
 }
 
 # log_likelihood_mixture_multinomials defined in mixture_of_multinomials.R
