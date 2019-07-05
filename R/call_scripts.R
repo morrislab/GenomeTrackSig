51aaa1a8e6d82fef9fb1222cd25217bab1992df7
Cait <cait.harrigan@mail.utoronto.ca>
2019-07-05 16:31:09 -0400

    add simulation mutTypes grab + formatting

--- R/call_scripts.R
+++ R/call_scripts.R
@@ -53,6 +53,58 @@ vcfToCounts <- function(vcfFile, cnaFile = NULL, purityFile = NULL,
 
 }
 
+#' @export
+vcfToCounts_simulation <- function(vcfFile, mutTypesFile, cnaFile = NULL, purityFile = NULL,
+                        context = generateContext(c("CG", "TA")), refGenome = BSgenome.Hsapiens.UCSC.hg19, binSize = 100,
+                        saveIntermediate = F, intermediateFile = NULL) {
+
+  # load CNA and purity dataframe (not loaded with VCF for parallelization memory saving)
+  # could be done as a single annotation load.... one function to load each file
+  # loads the following - all shared between all VCF's, all optional (but not necessarily independent)
+  # cna, purity, tumortypes, signatures (alex, cosmic), trinucleotide, sigactivities
+  # tumortype_file = "", signature_file = "", trinucleotide_file = "", active_signatures_file = ""
+
+  # input checking and path expansion
+
+
+  vcfFile <- path.expand(vcfFile)
+  stopifnot(file.exists(vcfFile))
+
+  if (!is.null(cnaFile)){
+    cnaFile <- path.expand(cnaFile)
+    stopifnot(file.exists(cnaFile))
+  }
+
+  if (!is.null(purityFile)){
+    purityFile <- path.expand(purityFile)
+    stopifnot(file.exists(purityFile))
+  }
+
+  # vcaf has vcf and vaf data concatenated
+  vcaf <- getVcaf(vcfFile, cnaFile, purityFile, refGenome)
+  mutTypes <- read.delim(mutTypesFile, stringsAsFactors = F)
+
+  # strip chr if present
+  mutTypes$chromosome <- unlist(strsplit(mutTypes$chromosome, "chr"))[c(F, T)]
+
+  # match ordering
+  vcaf <- vcaf[order(vcaf$pos, vcaf$chr),]
+  mutTypes <- mutTypes[order(mutTypes$start, mutTypes$chromosome),]
+
+  # sanity check
+  stopifnot( all(vcaf$chr == mutTypes$chromosome) & all(vcaf$pos == mutTypes$start) )
+
+  # set trinucs and re-order by phi
+  vcaf$mutType <- mutTypes$tri
+  vcaf <- vcaf[order(vcaf$phi),]
+
+
+  return( getBinCounts(vcaf, binSize, context) )
+
+
+}
+
+
 #' \code{getVcaf} Take an input vcf file and annotation and make vaf data
 #'
 #' @rdname callScripts
--- R/load_counts.R
+++ R/load_counts.R
@@ -421,3 +421,100 @@ loadAndScoreIt_pcawg <- function(vcfFile, tumortypes, acronym,
 
   return (plotting)
 }
+
+loadAndScoreIt_simulation <- function(vcfFile, mutTypesFile, tumortypes, acronym,
+                                 cnaFile = NULL, purityFile = NULL,
+                                 saveIntermediate = F) {
+
+
+  # load_sample() returns (inorder)
+  # list(vcfData, vcf, phis, quadratic_phis, phis_sliding_window, assigns_phylo_nodes,
+  #      assigns_phylo_nodes_sw, acronym, window, tumor_id, phis_for_plot, bootstrap_vcfs, bootstrap_phis))
+
+  tumor_id <- strsplit( unlist(strsplit(vcfFile, "/"))[ length( strsplit(vcfFile, "/")[[1]] ) ] , ".vcf")[[1]]
+
+  if (saveIntermediate){
+    dir.create(paste0(TrackSig.options()$DIR_RESULTS, "/mut_types/"), showWarnings = F, recursive = T)
+  }
+
+  list[phis, quadPhis, counts] <- vcfToCounts_simulation(vcfFile, mutTypesFile = mutTypesFile, cnaFile = cnaFile, purityFile = purityFile,
+                                              refGenome = BSgenome.Hsapiens.UCSC.hg19,
+                                              saveIntermediate = saveIntermediate,
+                                              intermediateFile = paste0(TrackSig.options()$DIR_RESULTS, "/mut_types/", tumor_id, ".mut_types.txt"))
+
+  # following checking is all from within compute_signatures_for_all_examples()
+  # will throw a next error if check fails
+
+  if (TrackSig.options()$sig_amount == "onlyKnownSignatures") {
+    # Fit only known signatures
+    list[alex.t, matched_type, acronym] <- TrackSig:::get_signatures_for_current_sample(tumor_id, active_signatures.our_samples, alex, TrackSig.options()$noise_sig)
+  } else {
+    alex.t <- alex
+  }
+
+  if (is.null(acronym) || acronym == "") {
+    print(paste("ERROR: Cancer type not found for ", example))
+    next
+  }
+
+  if (is.null(alex.t))
+  {
+    print(paste0("No active signatures for sample", example, " ...."))
+    next
+  }
+
+  if (is.vector(alex.t)) {
+    next
+  }
+
+  if (sum(counts[apply(alex.t,1,sum) == 0,] != 0) != 0) {
+    print(paste0("Sample ", example, ": some trinucleotides have probability 0 under the model, but their count is non-zero. Sot the count vector is impossible under the model."))
+    next
+  }
+
+  # this is the work-horse part
+
+  dir_name <- paste0(TrackSig.options()$DIR_RESULTS, acronym, "/", tumor_id, "/")
+  suppressWarnings(dir.create(dir_name, recursive = T))
+
+  #method_name <- "iterativeChangePoints"
+
+  if (!file.exists(paste0(dir_name, "mixtures.csv")) || !file.exists(paste0(dir_name, "changepoints.txt")))
+  {
+    if (TrackSig.options()$changepoint_method == "PELT") {
+      list[changepoints, mixtures] <- TrackSig:::find_changepoints_pelt(counts, alex.t, phis, quadPhis)
+    } else {
+      list[bics, optimal, changepoints, mixtures] <- find_changepoints_over_all_signatures_one_by_one(counts, alex.t, n_signatures = ncol(alex.t))
+    }
+
+    write.csv(mixtures, file=paste0(dir_name, "mixtures.csv"))
+
+    n_col <- ifelse(length(changepoints) > 0, length(changepoints), 1)
+    write(changepoints, file=paste0(dir_name, "changepoints.txt"), ncolumns=n_col)
+  } else {
+    mixtures <- TrackSig:::read_mixtures(paste0(dir_name, "mixtures.csv"))
+    cp_file = paste0(dir_name, "changepoints.txt")
+    if (file.info(cp_file)$size == 1) {
+      changepoints <- c()
+    } else {
+      changepoints <- unlist(read.table(cp_file, header=F))
+    }
+  }
+
+
+  plot_name <- paste0(dir_name, "/", acronym, "_", tumor_id, "_", TrackSig.options()$sig_amount, TrackSig.options()$postfix, ".pdf")
+
+  #if (TrackSig.options()$PLOT_FULL_NAME)
+  #{
+  #  plot_name <- paste0(dir_name, "/", acronym, "_", data_method, "_multMix_fittedPerTimeSlice_", sig_amount, "_noPrior_", method_name, postfix, ".pdf")
+  #}
+
+
+  mark_cp <- !is.null(changepoints)
+  plotting <- TrackSig:::plot_signatures_real_scale(mixtures*100, plot_name=plot_name, phis = phis, mark_change_points=mark_cp, change_points=changepoints,
+                                                    transition_points = NULL)
+  plotting <- plotting[[1]]
+  plotting$data$id <- tumor_id
+
+  return (plotting)
+}
