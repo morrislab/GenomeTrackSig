25ac8a831003b4d13363c6114ca553949d4081f0
Cait <cait.harrigan@mail.utoronto.ca>
2019-07-09 13:38:09 -0400

    poisson score function (8.2.4)

--- R/score_functions.R
+++ R/score_functions.R
@@ -9,27 +9,46 @@ gaussian_ll <- function(phis, quad_phis, bin_size, ...){
   # ... allows for various function signatures
   # Score a segment using likelihood under normal
 
-  B <- length(phis)
-  S <- bin_size
+  N <- length(phis) * bin_size
   sigmasq <- mean(quad_phis) - mean(phis)^2
 
   if (mean(quad_phis) >= mean(phis)^2){
     warning(sprintf("mean quad_phis is %s, mean phis^2 is %s", mean(quad_phis), mean(phis)^2))
   }
 
-  scale <- max(sqrt(2 * pi * sigmasq), 10^-8, na.rm = T)
-
-  LL <- - (S * B * log(scale)) - (S * B / 2)
+  LL <- (-N / 2) * (log(2 * pi * sigmasq) + 1)
 
   return(LL)
 }
 
 # log_likelihood_mixture_multinomials defined in mixture_of_multinomials.R
-sum_gaussian_mixture_multinomials_ll <- function(phis, quad_phis, multinomial_vector,
+sum_gaussian_mixture_ll <- function(phis, quad_phis, multinomial_vector,
                                                  composing_multinomials, mixtures, bin_size, ...){
 
   return( sum(log_likelihood_mixture_multinomials(multinomial_vector, composing_multinomials, mixtures),
               gaussian_ll(phis, quad_phis, bin_size)) )
+}
+
+#Poisson likelihood maximization
+poisson_ll <- function(phis, quad_phis, bin_size, ...){
+  # phis read from counts file
+  # quadratic_phis read from quadratic phis file
+  # ... allows for various function signatures
+  # Score a segment using likelihood under normal
+
+  N <- length(phis) * bin_size
+  sigmasq <- mean(quad_phis)
+
+  LL <- (-N / 2) * (log(2 * pi * sigmasq))
+
+  return(LL)
+}
+
+sum_poisson_mixture_ll <- function(phis, quad_phis, multinomial_vector,
+                                    composing_multinomials, mixtures, bin_size, ...){
+
+  return( sum(log_likelihood_mixture_multinomials(multinomial_vector, composing_multinomials, mixtures),
+              poisson_ll(phis, quad_phis, bin_size)) )
 }
 
 # [END]
