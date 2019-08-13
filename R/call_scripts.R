338a374fb9675334bb4b142cb0467668bce5462f
Cait <cait.harrigan@mail.utoronto.ca>
2019-08-13 13:18:29 -0400

    loadVCF depricates call_scripts

--- R/call_scripts.R
+++ R/call_scripts.R
@@ -1,14 +1,5 @@
 # call_scripts.R
 
-#' \code{callScripts} Call the supporting (non-R) scipts in TrackSig
-#'
-#' \code{vcf_to_counts} From a supplied VCF file run the necessary scripts to get corrected_vaf and make_counts output. Return a multi-slotted object with useful dataframes.
-#'
-#' \code{run_simulation} depricated
-#'
-
-
-
 
 #' @export
 vcfToCounts_simulation <- function(vcfFile, mutTypesFile, cnaFile = NULL, purityFile = NULL,
@@ -24,6 +15,7 @@ vcfToCounts_simulation <- function(vcfFile, mutTypesFile, cnaFile = NULL, purity
   # input checking and path expansion
 
 
+  # input checking and path expansion
   vcfFile <- path.expand(vcfFile)
   stopifnot(file.exists(vcfFile))
 
@@ -37,8 +29,14 @@ vcfToCounts_simulation <- function(vcfFile, mutTypesFile, cnaFile = NULL, purity
     stopifnot(file.exists(purityFile))
   }
 
+  # get vcf
+  vcf <- parseVcfFile(vcfFile)
+
+  # get cna reconstruction
+  cnaRanges <- parseCnaFile(cnaFile)
+
   # vcaf has vcf and vaf data concatenated
-  vcaf <- getVcaf(vcfFile, cnaFile, purityFile, refGenome)
+  vcaf <- getVcaf(vcf, purity, cnaRanges, refGenome)
   mutTypes <- read.delim(mutTypesFile, stringsAsFactors = F)
 
   # strip chr if present
@@ -67,147 +65,8 @@ vcfToCounts_simulation <- function(vcfFile, mutTypesFile, cnaFile = NULL, purity
 }
 
 
-#' \code{getTrinuc} Get the trinucleotide context for each mutation in a vcaf data frame
-#' @rdname callScripts
-#' @name getTrinuc
-#'
-#' @param vcaf vcaf data frame
-#' @param refGenome reference BSgenome to use
-#' @param saveIntermediate boolean whether to save intermediate results (mutation types)
-#' @param intermediateFile file where to save intermediate results if saveIntermediate is True
-#' @return An updated vcaf data frame with trinucleotide context added for each mutation
-getTrinuc <- function(vcaf, refGenome, saveIntermediate = F, intermediateFile){
-  # replaces getMutationTypes.pl
-
-  print("making mutation types")
-
-  # input checking
-  assertthat::assert_that(class(refGenome) == "BSgenome")
-  assertthat::assert_that(is.logical(saveIntermediate))
-
-  if(missing(intermediateFile) | is.null(intermediateFile)){
-    assertthat::assert_that(saveIntermediate == F, msg = "please specify an intermediate file to save to, or set saveIntermediate = FALSE")
-  }
-
-  # get trinucleotide context in refrence
-  # strandedness should be forward
-  # concat to GRanges object
-  mutRanges <- GRanges( paste0("chr", vcaf$chr, ":", vcaf$pos - 1, "-", vcaf$pos + 1, ":+") )
-
-  # look up trinucleotide context
-  triNuc <- getSeq(refGenome, mutRanges)
-  vcaf$mutType <- as.character(triNuc)
-
-  # context matches ref?
-  # perl script ignored this and grabbed trinuc context regardless.
-  # Here will drop these rows and throw warning
-  mismatchedRef <- which(!(vcaf$ref == substr(vcaf$mutType, 2, 2)))
-
-  if (length(mismatchedRef) > 0){
-
-    #warning( sprintf("%s mutations dropped for vcf refrence allele not matching the selected reference genome" , length(mismatchedRef) ) )
-    #vcaf <- vcaf[-mismatchedRef,]
-    #context <- context[-mismatchedRef]
-
-    warning( sprintf("%s mutations do not have vcf refrence allele not matching the selected reference genome" , length(mismatchedRef) ) )
-    substr(vcaf$mutType, 2, 2) <- vcaf$ref
-  }
-
-  # remove mutations with "N" in refrence context
-  rmSet <- sapply(triNuc, FUN = BSgenome::hasOnlyBaseLetters)
-  if (sum(rmSet) > 0){
-
-    warning( sprintf("%s mutations dropped for uncertain identity in reference genome" , sum(!rmSet)) )
-    vcaf <- vcaf[rmSet,]
-  }
-
-  # take reverse complement of ref purines for context format
-  complementSel <- (vcaf$ref == "G" | vcaf$ref == "A")
-  vcaf$mutType[complementSel] <- as.character(reverseComplement(DNAStringSet(vcaf$mutType))[complementSel])
-
-  # complement alt and ref where ref is a purine
-  vcaf$alt[vcaf$ref == "G"] <- as.character(complement(DNAStringSet(vcaf$alt[vcaf$ref == "G"])))
-  vcaf$alt[vcaf$ref == "A"] <- as.character(complement(DNAStringSet(vcaf$alt[vcaf$ref == "A"])))
-  vcaf$ref[vcaf$ref == "G"] <- "C"
-  vcaf$ref[vcaf$ref == "A"] <- "T"
-
-  if (saveIntermediate == TRUE){
-    mut_types <- vcaf[,c("chr", "pos", "phi", "ref", "alt", "mutType")]
-    write.table(mut_types, file = intermediateFile, quote = F, col.names = F, row.names = F, sep = "\t")
-  }
-
-  return (vcaf)
-}
-
-#' \code{getBinCounts} Get the mutation type counts data for a vcaf dataframe
-#'
-#' @rdname callScripts
-#' @name getBinCounts
-#'
-#' @param vcaf vcaf data frame
-#' @param binSize number of mutations per bin
-#' @param context trinucleotide combinations possible
-#' @return A data frame of summary statistics and mutation type counts for each bin.
-getBinCounts <- function(vcaf, binSize, context){
-  # replaces make_hundreds.py script
-
-  print("making counts")
-
-  nMut <- dim(vcaf)[1]
-  assertthat::assert_that(nMut > binSize, msg = "number of mutations may not be less than specified bin size")
-  assertthat::assert_that(dim(unique(vcaf[c("ref","alt","mutType")]))[1] <= dim(context)[1], msg = sprintf("too many mutation types (%s) for context (%s)",
-                          dim(unique(vcaf[c("ref","alt","mutType")]))[1],  dim(context)[1]) )
-
-  #nBins <- (nMut / binSize) + (nMut %/% binSize > 0)
-  nBins <- floor( (nMut / binSize) )
-
-  # only filling as many complete bins as we can
-  # up to the last (binSize - 1) mutations of smallest phi may be excluded.
-  vcaf$binAssignment <- c(rep(1:nBins, each = binSize), rep(NA, nMut - nBins * binSize))
-
-  # aggregate on bins
-  binCounts <- data.frame(row.names = 1:nBins)
-
-  # counts for each bin
-  binCounts <- cbind (binCounts, aggregate(paste(vcaf$ref, vcaf$alt, vcaf$mutType, sep = "_"), by = list(vcaf$binAssignment), FUN = function(x){return(as.array(table(x)))})$x )
-
-  # check that all mutation types have a count
-  missingTypes <- setdiff(paste(context$V1, context$V2, context$V3, sep = "_"), names(binCounts))
-  for (col in missingTypes){
-    binCounts[col] <- 0
-  }
-
-  return ( list(vcaf, t(binCounts)) )
-
-}
 
 
-
-#' @rdname callScripts
-#' @name run_simulation
-#'
-#' @export
-
-run_simulation <- function(simName, dataDir){
-
-  fileName <- system.file("scripts", "run_simulations.sh", package = "TrackSig")
-  packagePath <- system.file(package = "TrackSig")
-
-  system(sprintf("%s %s %s %s/%s %s %s", fileName, packagePath, dataDir, dataDir, simName, simName, TrackSig.options()$bin_size))
-
-}
-
-run_sample <- function(sampleName){
-
-  #fileName <- system.file("scripts", "run_simulations.sh", package = "TrackSig")
-  #packagePath <- system.file(package = "TrackSig")
-
-  #system(sprintf("%s %s data/mut_types/ data/%s %s", fileName, packagePath, simName, simName))
-
-  warning("run_sample not implemented")
-  return(NULL)
-
-}
 
 
 
--- R/loadVCAF.R
+++ R/loadVCAF.R
@@ -7,21 +7,18 @@
 #' @rdname callScripts
 #' @name vcfToCounts
 #'
-#' @param vcfFile path to variant calling format (vcf) file
-#' @param cnaFile path to copy number abberation (cna) file
-#' @param purityFile path to sample purity file
+#' @param vcfFile path to variant calling format (VCF) file
+#' @param cnaFile path to copy number abberation (CNA) file
+#' @param purity sample purity percentage between 0 and 1
+#' @param binSize size of TrackSig timeline bins
+#' @param context list of mutation types and their trinucleotide context
+#' @param refGenome refrence genome used to create VCF file
 #'
 #' @export
 vcfToCounts <- function(vcfFile, cnaFile = NULL, purity = 1, binSize = 100,
                         context = generateContext(c("CG", "TA")),
-                        refGenome = BSgenome.Hsapiens.UCSC.hg19,
-                        saveIntermediate = F, intermediateFile = NULL) {
+                        refGenome = BSgenome.Hsapiens.UCSC.hg19) {
 
-  # load CNA and purity dataframe (not loaded with VCF for parallelization memory saving)
-  # could be done as a single annotation load.... one function to load each file
-  # loads the following - all shared between all VCF's, all optional (but not necessarily independent)
-  # cna, purity, tumortypes, signatures (alex, cosmic), trinucleotide, sigactivities
-  # tumortype_file = "", signature_file = "", trinucleotide_file = "", active_signatures_file = ""
 
   # input checking and path expansion
   vcfFile <- path.expand(vcfFile)
@@ -41,11 +38,75 @@ vcfToCounts <- function(vcfFile, cnaFile = NULL, purity = 1, binSize = 100,
   vcf <- parseVcfFile(vcfFile)
 
   # get cna reconstruction
-  cna <- parseCnaFile(cnaFile)
+  cnaRanges <- parseCnaFile(cnaFile)
 
   # vcaf has vcf and vaf data concatenated
-  vcaf <- getVcaf(vcf, purity, cna, refGenome)
-  vcaf <- getTrinuc(vcaf, refGenome, saveIntermediate, intermediateFile)
+  vcaf <- getVcaf(vcf, purity, cnaRanges, refGenome)
+  vcaf <- getTrinuc(vcaf, refGenome)
+
+  return( getBinCounts(vcaf, binSize, context) )
+
+
+}
+
+
+#' @export
+vcfToCounts_simulation <- function(vcfFile, mutTypesFile, cnaFile = NULL, purityFile = NULL,
+                                   context = generateContext(c("CG", "TA")), refGenome = BSgenome.Hsapiens.UCSC.hg19, binSize = 100,
+                                   saveIntermediate = F, intermediateFile = NULL) {
+
+  # load CNA and purity dataframe (not loaded with VCF for parallelization memory saving)
+  # could be done as a single annotation load.... one function to load each file
+  # loads the following - all shared between all VCF's, all optional (but not necessarily independent)
+  # cna, purity, tumortypes, signatures (alex, cosmic), trinucleotide, sigactivities
+  # tumortype_file = "", signature_file = "", trinucleotide_file = "", active_signatures_file = ""
+
+  # input checking and path expansion
+
+
+  # input checking and path expansion
+  vcfFile <- path.expand(vcfFile)
+  stopifnot(file.exists(vcfFile))
+
+  if (!is.null(cnaFile)){
+    cnaFile <- path.expand(cnaFile)
+    stopifnot(file.exists(cnaFile))
+  }
+
+  if (!is.null(purityFile)){
+    purityFile <- path.expand(purityFile)
+    stopifnot(file.exists(purityFile))
+  }
+
+  # get vcf
+  vcf <- parseVcfFile(vcfFile)
+
+  # get cna reconstruction
+  cnaRanges <- parseCnaFile(cnaFile)
+
+  # vcaf has vcf and vaf data concatenated
+  vcaf <- getVcaf(vcf, purity, cnaRanges, refGenome)
+  mutTypes <- read.delim(mutTypesFile, stringsAsFactors = F)
+
+  # strip chr if present
+  mutTypes$chromosome <- unlist(strsplit(mutTypes$chromosome, "chr"))[c(F, T)]
+
+  # match ordering
+  vcaf <- vcaf[order(vcaf$pos, vcaf$chr),]
+  mutTypes <- mutTypes[order(mutTypes$start, mutTypes$chromosome),]
+
+  # sanity check
+  stopifnot( all(vcaf$chr == mutTypes$chromosome) & all(vcaf$pos == mutTypes$start) )
+
+  # set trinucs and re-order by phi
+  vcaf$mutType <- mutTypes$tri
+  vcaf <- vcaf[order(vcaf$phi),]
+
+  # save intermediate is necessary
+  if (saveIntermediate == TRUE){
+    mut_types <- vcaf[,c("chr", "pos", "phi", "ref", "alt", "mutType")]
+    write.table(mut_types, file = intermediateFile, quote = F, col.names = F, row.names = F, sep = "\t")
+  }
 
   return( getBinCounts(vcaf, binSize, context) )
 
@@ -155,8 +216,7 @@ getVcaf <- function(vcf, purity, cna, refGenome = BSgenome.Hsapiens.UCSC.hg19){
   # prelim formatting check
   vcaf <- vcafConstruction(vcf, refGenome)
 
-  vcaf$vi <- info(vcf)$t_alt_count
-  vcaf$ri <- info(vcf)$t_ref_count
+  # populate vcaf with phi calculations
   vcaf$purity <- purity
   vcaf$phat1 <- rbeta(dim(vcaf)[1], vcaf$vi + 1, vcaf$ri + 1)
   vcaf$phat2 <- rbeta(dim(vcaf)[1], vcaf$vi + 1, vcaf$ri + 1)
@@ -181,7 +241,7 @@ getVcaf <- function(vcf, purity, cna, refGenome = BSgenome.Hsapiens.UCSC.hg19){
 #' @param refGenome reference BSgenome to use
 #' @return A vcaf dataframe that has vcf and vaf data concatenated
 
-vcafConstuction <- function(vcf, refGenome = BSgenome.Hsapiens.UCSC.hg19){
+vcafConstruction <- function(vcf, refGenome = BSgenome.Hsapiens.UCSC.hg19){
   # some VCF formatting checks, filter for SNP's
   # no read quality filtering performed.
 
@@ -197,64 +257,167 @@ vcafConstuction <- function(vcf, refGenome = BSgenome.Hsapiens.UCSC.hg19){
 
   if (sum(rmSel) > 0){
     warning( sprintf("%s mutations dropped for not meeting SNP cirteria" , sum(rmSel) ) )
-    vcaf <- vcaf[!rmSel,]
+    vcf <- vcf[!rmSel,]
   }
 
   # formatting vcaf - vcf and vaf concatenated
   # using CharacterList for alt, ref is 400x faster than casting CollapsedVCF object directly
   # subset with [0,] to avoid casting metadata columns
   vcaf <- as.data.frame(rowRanges(vcf)[,0])[c("seqnames", "start")]
+  colnames(vcaf) <- c("chr", "pos")
 
-  # take first variant of those with multiallelic alternate variant
+  vcaf$ref <- unlist(CharacterList(list(rowRanges(vcf)$REF)))
+
+  # drop secondary alleles in multiallelic alt hits
   allAlts <- CharacterList(rowRanges(vcf)$ALT)
   vcaf$alt <- unlist(lapply(allAlts, function(x){return(x[[1]][1])}))
 
-  # want alt and ref counts for all loci
-  assertthat::assert_that(dim(vcaf)[1] == length(info(vcf)$t_alt_count), dim(vcaf)[1] == length(info(vcf)$t_ref_count))
-  colnames(vcaf) <- c("chr", "pos", "ref", "alt", "cn")
+  # check - names and dimenions should match
+  assertthat::assert_that( all( rownames(vcaf) == names(vcf) ) )
+  assertthat::assert_that( dim(vcaf)[1] == dim(info(vcf))[1] )
 
+  # get copy number for all loci
+  vcaf$cn <- info(vcf)$cn
+
+  # get alt and ref counts for all loci
   # TODO: vcf formats may provide vi and depth, rather than vi and ri
+  vcaf$vi <- info(vcf)$t_alt_count
+  vcaf$ri <- info(vcf)$t_ref_count
 
-  # drop secondary alleles in multiallelic alt hits
-  oneCharAllele <- function(alt){alt[[1]][1]}
-
-  # ref should not match alt in a mutation
+  # check - ref should not match alt in a mutation
   rmSel <- vcaf$ref == vcaf$alt
   if (sum(rmSel) > 0){
-    warning(sprintf("%s mutations dropped for refrence allele matching alt", length(rmSet)))
+    warning(sprintf("%s mutations dropped for refrence allele matching alt", sum(rmSel)))
     vcaf <- vcaf[!rmSel,]
   }
 
-  # mutation should be a SNP
+  # check - mutations should be SNP
   rmSet <- c()
 
-  # lists of >2 alt alleles not SNP
-  # don't count incluce python list characters - [,]
-  rmSet <- union(rmSet, which(nchar(vcaf$alt) > 5))
-
-  # lists of >1 ref allele not SNP
-  # don't count incluce python list characters - [,]
-  rmSet <- union(rmSet, which(nchar(vcaf$ref) > 3))
-
-  # chromosome should be valid in refrence genome
-  # don't load genome - use BSgenome previewe accessing
-  # "chr" is stripped by make_vcaf so return for matching with BSgenome
-  rmSet <- union(rmSet, which(!(paste0("chr", vcaf$chr) %in% seqnames(refGenome))))
+  # check - chromosome should be valid in refrence genome
+  # don't load genome - use BSgenome preview accessing
+  rmSet <- union(rmSet, which(!(vcaf$chr %in% seqnames(refGenome))))
 
   # postition should be valid in refrence genome
   # not less than 1
   rmSet <- union(rmSet, which( vcaf$pos < 1 ) )
-
   #and less than the maximum for that chromosome
   rmSet <- union(rmSet, which ( ! ( vcaf$pos < seqlengths(refGenome)[paste0("chr", vcaf$chr)] ) ))
 
   if (length(rmSet) > 0){
-    warning( sprintf("%s mutations dropped for not meeting SNP cirteria" , length(rmSet) ) )
+    warning( sprintf("%s mutations dropped for not appearing in reference genome" , length(rmSet) ) )
     vcaf <- vcaf[-rmSet,]
   }
+
+  # strip "chr" for downstream
+  vcaf$chr <- as.character(vcaf$chr)
+  vcaf$chr <- unlist(strsplit(vcaf$chr, "chr"))[c(F, T)]
 
   return ( vcaf )
 }
 
+#' \code{getTrinuc} Get the trinucleotide context for each mutation in a vcaf data frame
+#' @rdname callScripts
+#' @name getTrinuc
+#'
+#' @param vcaf vcaf data frame
+#' @param refGenome reference BSgenome to use
+#' @param intermediateFile file where to save intermediate results if saveIntermediate is True
+#' @return An updated vcaf data frame with trinucleotide context added for each mutation
+getTrinuc <- function(vcaf, refGenome){
+  # replaces getMutationTypes.pl
+
+  print("making mutation types")
+
+  # input checking
+  assertthat::assert_that(class(refGenome) == "BSgenome")
+
+  # get trinucleotide context in refrence
+  # strandedness should be forward
+  # concat to GRanges object
+  mutRanges <- GRanges( paste0("chr", vcaf$chr, ":", vcaf$pos - 1, "-", vcaf$pos + 1, ":+") )
+
+  # look up trinucleotide context
+  triNuc <- getSeq(refGenome, mutRanges)
+  vcaf$mutType <- as.character(triNuc)
+
+  # context matches ref?
+  # perl script ignored this and grabbed trinuc context regardless.
+  # Here will drop these rows and throw warning
+  mismatchedRef <- which(!(vcaf$ref == substr(vcaf$mutType, 2, 2)))
+
+  if (length(mismatchedRef) > 0){
+
+    #warning( sprintf("%s mutations dropped for vcf refrence allele not matching the selected reference genome" , length(mismatchedRef) ) )
+    #vcaf <- vcaf[-mismatchedRef,]
+    #context <- context[-mismatchedRef]
+
+    warning( sprintf("%s mutations have vcf refrence allele mismatch with the selected reference genome" , length(mismatchedRef) ) )
+    substr(vcaf$mutType, 2, 2) <- vcaf$ref
+  }
+
+  # remove mutations with "N" in refrence context
+  rmSet <- sapply(triNuc, FUN = BSgenome::hasOnlyBaseLetters)
+  if (sum(rmSet) > 0){
+
+    warning( sprintf("%s mutations dropped for uncertain identity in reference genome" , sum(!rmSet)) )
+    vcaf <- vcaf[rmSet,]
+  }
+
+  # take reverse complement of ref purines for context format
+  complementSel <- (vcaf$ref == "G" | vcaf$ref == "A")
+  vcaf$mutType[complementSel] <- as.character(reverseComplement(DNAStringSet(vcaf$mutType))[complementSel])
+
+  # complement alt and ref where ref is a purine
+  vcaf$alt[vcaf$ref == "G"] <- as.character(complement(DNAStringSet(vcaf$alt[vcaf$ref == "G"])))
+  vcaf$alt[vcaf$ref == "A"] <- as.character(complement(DNAStringSet(vcaf$alt[vcaf$ref == "A"])))
+  vcaf$ref[vcaf$ref == "G"] <- "C"
+  vcaf$ref[vcaf$ref == "A"] <- "T"
+
+
+  return (vcaf)
+}
+
+#' \code{getBinCounts} Get the mutation type counts data for a vcaf dataframe
+#'
+#' @rdname callScripts
+#' @name getBinCounts
+#'
+#' @param vcaf vcaf data frame
+#' @param binSize number of mutations per bin
+#' @param context trinucleotide combinations possible
+#' @return A data frame of summary statistics and mutation type counts for each bin.
+getBinCounts <- function(vcaf, binSize, context){
+  # replaces make_hundreds.py script
+
+  print("making counts")
+
+  nMut <- dim(vcaf)[1]
+  assertthat::assert_that(nMut > binSize, msg = "number of mutations may not be less than specified bin size")
+  assertthat::assert_that(dim(unique(vcaf[c("ref","alt","mutType")]))[1] <= dim(context)[1], msg = sprintf("too many mutation types (%s) for context (%s)",
+                                                                                                           dim(unique(vcaf[c("ref","alt","mutType")]))[1],  dim(context)[1]) )
+
+  #nBins <- (nMut / binSize) + (nMut %/% binSize > 0)
+  nBins <- floor( (nMut / binSize) )
+
+  # only filling as many complete bins as we can
+  # up to the last (binSize - 1) mutations of smallest phi may be excluded.
+  vcaf$binAssignment <- c(rep(1:nBins, each = binSize), rep(NA, nMut - nBins * binSize))
+
+  # aggregate on bins
+  binCounts <- data.frame(row.names = 1:nBins)
+
+  # counts for each bin
+  binCounts <- cbind (binCounts, aggregate(paste(vcaf$ref, vcaf$alt, vcaf$mutType, sep = "_"), by = list(vcaf$binAssignment), FUN = function(x){return(as.array(table(x)))})$x )
+
+  # check that all mutation types have a count
+  missingTypes <- setdiff(paste(context$V1, context$V2, context$V3, sep = "_"), names(binCounts))
+  for (col in missingTypes){
+    binCounts[col] <- 0
+  }
+
+  return ( list(vcaf, t(binCounts)) )
+
+}
 
 # [END]
