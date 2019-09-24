9d1313982061cb0a988fe59035b657591ed69cb1
Cait <cait.harrigan@mail.utoronto.ca>
2019-09-24 11:59:50 -0400

    allow diff bin size

--- R/TrackSig.R
+++ R/TrackSig.R
@@ -83,7 +83,7 @@ TrackSig <- function(vcfFile,
   # side effect: plot
   tryCatch({
             plot_name <- paste0(sampleID, " Signature Trajectory")
-            binned_phis <- aggregate(vcaf$phi, by = list(vcaf$bin), FUN = sum)$x / binSize
+            binned_phis <- aggregate(vcaf$phi, by = list(vcaf$bin), FUN = mean)$x
             mark_cp <- !is.null(changepoints)
             print(plot_signatures_real_scale(mixtures * 100, plot_name=plot_name, phis = binned_phis, mark_change_points=mark_cp,
                                        change_points=changepoints, transition_points = NULL, save = F)[[1]])
